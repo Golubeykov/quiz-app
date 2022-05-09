d7684aa300a8127faf1dbef579acaa1df45ad281
GolubeykovAnton <ds6580@yandex.ru>
2022-05-09 12:46:31 +0300

    Initial Commit

---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by Антон Голубейков on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
